b3377fa69aee0eab42f83aaa5de46338f2b93b70
Ivan <ivanfilho746@gmail.com>
2026-05-23 16:40:32 -0300

    feat: change databse to postgreesql and create schema.prisma

--- src/index.ts
+++ src/index.ts
@@ -1,54 +1,13 @@
 import fastify from "fastify";
-import type { FastifyRequest, FastifyReply } from "fastify"
 import cors from "@fastify/cors"
-import { prisma } from "../lib/prisma";
+import { userRoutes } from "./user";
 
 const app = fastify()
 
 app.register(cors, { origin: "*" })
+await app.register(userRoutes)
 
-type CreateUserBody = {
-	name: string;
-	email: string;
-};
 
-app.get("/users", (req: FastifyRequest, reply: FastifyReply) => {
-	async function getUsers() {
-		const users = await prisma.user.findMany()
-		reply.status(200).send(users);
-	}
-	getUsers()
-		.then(async () => {
-			await prisma.$disconnect();
-		})
-		.catch(async (e) => {
-			console.error(e);
-			await prisma.$disconnect();
-			process.exit(2);
-		});
-})
-
-app.post<{ Body: CreateUserBody }>("/createUser", (req, reply) => {
-	async function createUser() {
-		const { name, email } = req.body
-		const user = await prisma.user.create({
-			data: {
-				name: name,
-				email: email
-			}
-		})
-		reply.status(200).send(`User: ${name}, with ${email} created`)
-	}
-	createUser().then(async () => {
-		await prisma.$disconnect();
-	})
-		.catch(async (e) => {
-			console.error(e);
-			await prisma.$disconnect();
-			process.exit(2);
-		});
-
-})
 await app.listen({ port: 3333, host: "0.0.0.0" }).then(() => {
 	console.log("server running on port 3333")
 }).catch(error => {
